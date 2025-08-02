68ec88217ea22da130cca8f593d7b7ce729a6b48
StraightUpMoss <161911274+StraightUpMoss@users.noreply.github.com>
2025-08-02 16:37:22 -0500

    Upload from desktop

---
+++ Patch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using DuplicateUpgradesAllowed.Patches;
+using UnityEngine;
+
+namespace DuplicateUpgradesAllowed 
+{
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    public class Patch : BaseUnityPlugin
+    {
+        private const string ModGuid = "Poseidon.DuplicateUpgradesAllowed";
+        private const string ModName = "Duplicate Upgrades Allowed";
+        private const string ModVersion = "1.0.0";
+        private readonly Harmony _harmony = new Harmony(ModGuid);
+        
+
+        private ManualLogSource _mls;
+
+        private void Awake()
+        {
+            _mls = BepInEx.Logging.Logger.CreateLogSource(ModGuid);
+            Debug.Log($"{ModName}, {ModVersion} Active");
+            _mls.LogMessage($"{ModName}, {ModVersion} Active");
+            try{_harmony.PatchAll(typeof(StackableUpgradesPatch));}
+            catch (Exception e) {Debug.Log(e);}
+        }
+    }
+}
