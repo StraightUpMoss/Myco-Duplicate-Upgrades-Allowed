b7f12bfca843a28131c0894ca8902cfb1994a36f
StraightUpMoss <161911274+StraightUpMoss@users.noreply.github.com>
2025-08-02 17:20:39 -0500

    Add files via upload

---
+++ StackableUpgradesPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace DuplicateUpgradesAllowed.Patches
+{
+    [HarmonyPatch(typeof(ModuleEquipSlots))]
+    public class StackableUpgradesPatch
+    {
+        private static HexMap tempHex;
+        [HarmonyPatch("CancelStackingEquip")]
+        [HarmonyPrefix]
+        public static void CancelStackingEquipPatchPre(ref HexMap ___hexMap)
+        {
+            tempHex = ___hexMap;
+            ___hexMap = new HexMap();
+        }
+        [HarmonyPatch("CancelStackingEquip")]
+        [HarmonyPostfix]
+        public static void CancelStackingEquipPatchPost(ref HexMap ___hexMap)
+        {
+            ___hexMap = tempHex;
+        }
+    }
+}
